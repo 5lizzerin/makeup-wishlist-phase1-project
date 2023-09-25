b302a52899287e134aa0731772e848a4984fe4c7
Lizz <lizzcklein@gmail.com>
2023-09-25 15:57:28 -0400

    added IDs to HTML and started fetching data in index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-//javascript code will go here
 
-console.log("testing")
+const makeupUrl = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
+fetch(makeupUrl)
+.then(makeupUrlResponse => makeupUrlResponse.json())
+.then(productData =>{
+    
+})
+
+
+//declaring items in the card
+const makeupImage = document.getElementById("makeup-image")
+const makeupName = document.getElementById("makeup-name")
+const makeupPrice = document.getElementById("makeup-price")
