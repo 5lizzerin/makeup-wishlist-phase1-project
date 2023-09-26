f7152444e4b4144f38e24a307d615f32fd45e702
Justine <Justinegeechu333@gmail.com>
2023-09-26 08:30:20 -0500

    udpated1

--- index.js
+++ index.js
@@ -16,7 +16,26 @@ fetch(makeupUrl)
 			const productPrice = document.createElement('p');
 			productPrice.innerText = record.price;
 
-			card.append(productImage, productName, productPrice);
+			const likeBtn = document.createElement('button');
+			likeBtn.setAttribute('class', 'like-btn');
+			likeBtn.innerHTML = `
+			<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="2rem"
+					height="2rem"
+					viewBox="0 0 64 64"
+					version="1.1"
+				>
+					<path
+						d="m21.241 7.1495c-4.0125 1e-7 -7.5565 1.5303-10.631 4.5861-5.8865 5.851-6.1348 15.13-0.7502 21.273-0.139-0.056 22.098 22.046 22.154 21.991-0.056 0.055 22.182-22.047 22.126-21.991 5.385-6.143 5.136-15.422-0.75-21.273-5.882-5.8464-15.21-6.38-21.39-1.042-2.952-2.5567-7.088-3.5445-10.759-3.5445z"
+						fill="white"
+						stroke="#d92856"
+						stroke-width="2"
+					/>
+				</svg>
+			`;
+			card.append(productImage, productName, productPrice, likeBtn);
+
 			console.log(makeupContainer);
 			makeupContainer.append(card);
 		}
