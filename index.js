fb66679189331f94675959cdf9921ed2ffe3e2c9
Lizz <lizzcklein@gmail.com>
2023-09-26 09:36:27 -0400

    nothing was really changed

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 
 const makeupUrl = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
+
+
 fetch(makeupUrl)
-.then(makeupUrlResponse => makeupUrlResponse.json())
-.then(productData => {
-    // console.log(productData)
-    for (record of productData){
-        const card = document.createElement("div")
+ .then(makeupUrlResponse => makeupUrlResponse.json())
+ .then(productData => {
+     console.log(productData)
+     for (record of productData){
+         const card = document.createElement("div")
 
-        const productName = document.createElement("h2")
-        productName.innerText = record.name
+         const productName = document.createElement("h2")
+         productName.innerText = record.name
 
-        const productImage = document.createElement("img")
-        productImage.src = record.image_link
+         const productImage = document.createElement("img")
+         productImage.src = record.image_link
 
-        const productPrice = document.createElement("p")
-        productPrice.innerText = record.price
+         const productPrice = document.createElement("p")
+         productPrice.innerText = record.price
 
-        card.append(productImage, productName, productPrice)
-        console.log(makeupContainer)
-        makeupContainer.append(card)
-    }
+         card.append(productImage, productName, productPrice)
+         console.log(makeupContainer)
+         makeupContainer.append(card)
+     }
 })
-
 
 const makeupContainer = document.querySelector(".makeup-container")
 
